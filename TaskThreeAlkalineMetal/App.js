2e6c717e8b4023f2f38420226788099759355e85
Stephen Thomas <stephen.thomas042306@gmail.com>
2023-01-29 20:08:41 -0600

    centered i think
    project in stored under aashman c drive therefore i cant run it. However i think the buttons are centered.

--- TaskThreeAlkalineMetal/App.js
+++ TaskThreeAlkalineMetal/App.js
@@ -11,60 +11,62 @@ export default function App() {
     <SafeAreaView style={styles.container}>
       <ScrollView  style={styles.scrollView}>
       <Text style={styles.title}>Alkaline Metals</Text>
-      <ElementButton
-          name = "Lithium"
-          onPress = {() => console.log("works!")}
-          backColor = "yellow"
-          symbol= "Li"
-          num1="3"
-          num2 ="7.0"
-          underColor= "red"
-          />
+      <View style = {styles.elements}>
         <ElementButton
-          name = "Sodium"
-          symbol="Na"
-          num1="11"
-          num2 = "22.98"
-          onPress = {() => console.log("works!")}
-          backColor = "yellow"
-          underColor= "red"
-          />
+            name = "Lithium"
+            onPress = {() => console.log("works!")}
+            backColor = "yellow"
+            symbol= "Li"
+            num1="3"
+            num2 ="7.0"
+            underColor= "red"
+            />
           <ElementButton
-          name = "Potassium"
-          symbol="K"
-          num1="19"
-          num2 = "39.038"
-          onPress = {() => console.log("works!")}
-          backColor = "yellow"
-          underColor= "red"
-          />
-          <ElementButton
-          name = "Rubidium"
-          symbol="Rb"
-          num1="37"
-          num2 = "85.47"
-          onPress = {() => console.log("works!")}
-          backColor = "yellow"
-          underColor= "red"
-          />
-           <ElementButton
-          name = "Caesium"
-          symbol="Cs"
-          num1="55"
-          num2 = "132.91"
-          onPress = {() => console.log("works!")}
-          backColor = "yellow"
-          underColor= "red"
-          />
-          <ElementButton
-          name = "Francium"
-          symbol="Fr"
-          num1="87"
-          num2 = "223"
-          onPress = {() => console.log("works!")}
-          backColor = "yellow"
-          underColor= "red"
-          />
+            name = "Sodium"
+            symbol="Na"
+            num1="11"
+            num2 = "22.98"
+            onPress = {() => console.log("works!")}
+            backColor = "yellow"
+            underColor= "red"
+            />
+            <ElementButton
+            name = "Potassium"
+            symbol="K"
+            num1="19"
+            num2 = "39.038"
+            onPress = {() => console.log("works!")}
+            backColor = "yellow"
+            underColor= "red"
+            />
+            <ElementButton
+            name = "Rubidium"
+            symbol="Rb"
+            num1="37"
+            num2 = "85.47"
+            onPress = {() => console.log("works!")}
+            backColor = "yellow"
+            underColor= "red"
+            />
+            <ElementButton
+            name = "Caesium"
+            symbol="Cs"
+            num1="55"
+            num2 = "132.91"
+            onPress = {() => console.log("works!")}
+            backColor = "yellow"
+            underColor= "red"
+            />
+            <ElementButton
+            name = "Francium"
+            symbol="Fr"
+            num1="87"
+            num2 = "223"
+            onPress = {() => console.log("works!")}
+            backColor = "yellow"
+            underColor= "red"
+            />
+          </View>
     <View style = {styles.roundButton}></View>
     </ScrollView>
     </SafeAreaView>
@@ -77,6 +79,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     
   },
+
   scrollView: {
     backgroundColor: '#fff',
     marginHorizontal: 20,
@@ -90,6 +93,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     textDecorationLine: 'underline'
+  },
+
+  elements: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 50,
   }
 
 });
